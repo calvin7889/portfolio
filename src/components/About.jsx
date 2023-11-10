15497641e47012543d87ef7d76e631dec2faa022
Calvin <calvincheng2000@gmail.com>
2023-11-10 17:35:47 -0500

    feat: about done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,6 +3,7 @@ import ReactMarkdown from 'react-markdown';
 import { Container, Col, Row } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import Fade from 'react-reveal';
+import CrossfadeImage from 'react-crossfade-image';
 import Header from './Header';
 import endpoints from '../constants/endpoints';
 import FallbackSpinner from './FallbackSpinner';
@@ -43,6 +44,37 @@ function About(props) {
       .catch((err) => err);
   }, []);
 
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((index + 1) % 4);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [index]);
+
+  const isMobile = {
+    Android: function a() {
+      return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function b() {
+      return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function c() {
+      return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function d() {
+      return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function e() {
+      return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+    },
+    any: function f() {
+      return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS()
+          || isMobile.Opera() || isMobile.Windows());
+    },
+  };
   return (
     <>
       <Header title={header} />
@@ -54,10 +86,14 @@ function About(props) {
                 <Row>
                   <Col style={styles.introTextContainer}>
                     {parseIntro(data.about)}
+                    {isMobile.any() && <CrossfadeImage src={data?.imageSource[index]} duration={2000} style={{ height: '500px' }} alt="profile" />}
+
                   </Col>
-                  <Col style={styles.introImageContainer}>
-                    <img style={{ width: '700px' }} src={data?.imageSource} alt="profile" />
-                  </Col>
+                  { !isMobile.any() && (
+                    <Col style={styles.introImageContainer}>
+                      <CrossfadeImage src={data?.imageSource[index]} duration={2000} style={{ height: '500px' }} alt="profile" />
+                    </Col>
+                  )}
                 </Row>
               </Fade>
             )
